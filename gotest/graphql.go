a18fa25c1e76f6cb7dde2755fe123cf83ef0fa9b
sakapun <sakapun5th@gmail.com>
2018-07-17 02:24:53 +0900

    graphqlのクエリ

--- gotest/graphql.go
+++ gotest/graphql.go
@@ -1,12 +1,25 @@
 package main
 
 /*
+# Type queries into this side of the screen, and you will
+# see intelligent typeaheads aware of the current GraphQL type schema,
+# live syntax, and validation errors highlighted within the text.
+
+# We'll get you started with a simple query showing your username!
 query {
   repository(owner: "vuejs", name: "awesome-vue") {
-    pullRequests(last: 20) {
+    pullRequests(
+      states: [MERGED],
+      last: 20,
+      orderBy: {
+        field: UPDATED_AT,
+        direction: ASC
+      }
+    ) {
       nodes {
         title,
         merged,
+        mergedAt,
         url
       }
     }
