51a1646fb41617961c4efcd9a929068405bf181d
sakapun <sakapun5th@gmail.com>
2018-07-24 23:11:06 +0900

    レイアウトの整え

---
+++ components/AwesomeItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="box">
+    <div class="column">
+      <div>
+        <a :href="item.Url"
+         class="title"
+         target="_blank">{{item.Title}}</a>
+      </div>
+      <div class="subtitle">{{item.MergedAt | formatedDate}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import moment from "moment";
+  // const lang = navigator && navigator.language
+  // moment.locale(lang)
+  export default {
+    name: "AwesomeItem",
+    props: {
+      item: {
+        Title: {
+          type: String
+        },
+        Url: {
+          type: String
+        },
+        MergedAt: {
+          type: String
+        }
+      }
+    },
+    filters: {
+      formatedDate(date) {
+        return moment(date).format("ll");
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
